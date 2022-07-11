78c415cef37d9da5d939620d9b67affcdec672bc
anna maslova <zorina.a.1@yandex.ru>
2022-07-11 11:58:10 +0300

    make some changes

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,4 +1,8 @@
 ﻿Console.Write("Введите ваше имя");
 string username = Console.ReadLine();
 Console.Write("Привет, ");
+
+NewMethod(username);
+
+static void NewMethod(string username)
 Console.Write(username);
